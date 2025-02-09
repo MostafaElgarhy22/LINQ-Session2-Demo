5eafc4550d406fe11bcd411eff3af44b8bf0b9f5
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-02-09 23:07:47 +0200

    The Rest Of Topics

--- LINQ Session2 Demo/Program.cs
+++ LINQ Session2 Demo/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LINQ_Session2_Demo
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+namespace LINQ_Session2_Demo
 {
     internal class Program
     {
@@ -119,6 +121,139 @@
             #endregion
 
             #endregion
+
+            #region Conversion (Casting) Operators - Immediate Execution
+
+            // List<Product> List = ProductList.Where(P => P.UnitStock == 0).ToList();
+
+            // Product[] Array = ProductList.Where(P => P.UnitStock == 0).ToArray();
+
+            // Dictionary < long, Producct> Dictionary01 = ProductList.Where (P => P.UnitStock == 0)
+            // .ToDictionary(P => P.ProductID);
+
+            // Dictionary < long, Producct> Dictionary02 = ProductList.Where (P => P.UnitStock == 0)
+            // .ToDictionary(P => P.ProductID, P => P.ProductName);
+
+            // Dictionary02 = ProductList.Where(P => P.UnitStock == 0).ToHashSet();
+
+            // HashSet<Product> HashSet = ProductList.Where(P => P.UnitStock == 0).ToHashSet();
+
+            // hashSet<Product> HashSet = ProductList.Where(P => P.UnitStock == 0).ToHashSet(new ProductComparer());
+
+            // var sortedCollection = ProductList.Where(P => P.UnitStock == 0).ToImmutableSortedSet();
+
+
+            #endregion
+
+            #region Generation Operators
+
+            // The Only Way for calling Theses Methods (Operators) is by using Enumerable Class
+
+            // var Result = Enumerable.Range(0, 100); // 0 to 99
+
+            // var Result = Enumerable.Repeat<Product> (new Product(), 100);
+
+            // var Result = Enumerable.Empty<Product>();
+
+            // foreach (var item in Result)
+            // Console.WriteLine(item);
+            #endregion
+
+            #region Set Operators - Union Family Operators
+
+
+            // var Seq01 = Enumerable.Range(0, 100); // 0 to 99
+            // var Seq02 = Enumerable.Range(50, 100); // 50 to 149
+
+            // var Result = Seq01.Union(Seq02); // Merging the two sequences and removing the duplicates
+
+            // Result = Seq01.Concat(Seq02); // Merging the two sequences without removing the duplicates
+
+            // Result = Seq01.Distinct(); // Removing the duplicates from the sequence
+
+            // Result = Seq01.Intersect(Seq02); // [50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
+
+            // Result = Seq01.Except(Seq02); [0 To 49]
+
+
+            // foreach (var item in Result)
+            // Console.WriteLine(item); 
+
+            #endregion
+
+            #region Quantifier Operators - Return Boolean Value
+
+
+            // Console.Writeline(
+            // ProductList.Any()
+            // ProductList.Any(P=> P.UnitInstock == 0 )
+            // ProductList.All(P=> P.UnitInstock > 0 )
+            //ProductList.Contains(Product)
+            //ProductList.Contains(Product , new ProductEqualityComparer())
+            // ); 
+
+            #endregion
+
+            #region Transformation Operators - Zip
+
+            // List<string> Words = new List<string>() {"Ten" , "Twenty" ,"Thirty" , "Fourty" };
+            // int [] Numbers = [10,20,30,40,50,60,70];
+
+            //var Result = NumberS.Zip(words);
+            //var Result = NumberS.Zip(words , (number , word) => $" {number} = {word} ");
+            //var Result = NumberS.Zip(words , [1, 2, 3]); // C# 10.0 New Feature.
+
+            // foreach(var item in Result)
+            // Console.WriteLine(item);
+            #endregion
+
+            #region Partitioning Operators - Take, Skip , TakeLast, SkipLast , TakeWhile , SkipWhile
+
+            #region Take , TakeLast , Skip , SkipLast
+
+            // var Result = ProductList.Where (P => P.UnitsInStock > 0).Take(5);
+            //  Result = ProductList.Where (P => P.UnitsInStock > 0).Take(5);
+
+            //  Result = ProductList.Where (P => P.UnitsInStock > 0).Skip(5);
+            //  Result = ProductList.Where (P => P.UnitsInStock > 0).SkipLast(5);
+
+            //var PageSize = 10;
+            //var pageIndex = 4;
+
+
+
+
+            // foreach(var product in Result)
+            //  Console.Writeline(product); 
+            #endregion
+
+            #region TakeWhile, SkipWhile
+            // int [] Numbers = [1, 2, 3, 4, 5, 7 ,6, 9, 2];
+
+            // var Result = Numbers.Where((number) => number % 3 != 0);
+
+
+            // foreach(var product in Result)
+            //  Console.Writeline(product);  
+            #endregion
+            #endregion
+
+            #region Let / Into
+
+
+            //// aeiouAEIOU
+
+            //List<string> Names = new List<string>() { "Ahmed", "Mahmoud", "Rana", "Shahd" };
+
+            //var Result = from N in Names
+            //             select Regex.Replace(N, "[aeiouAEIOU]", string.Empty);
+
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
         }
     }
 }
